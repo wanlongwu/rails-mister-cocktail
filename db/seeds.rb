e4b046823283fa1b553ecc6e90f7b616e9bed256
Wanlong Wu <wumalone@gmail.com>
2018-02-23 13:02:16 +0900

    add ssed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "coke")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "red bull")
+Ingredient.create(name: "pineapple juice")
+
