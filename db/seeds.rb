727469711c19298dc903c4f2c942498bd219429c
Wanlong Wu <wumalone@gmail.com>
2018-02-23 14:59:33 +0900

    complete more ingredient in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,18 @@ Ingredient.create(name: "rum")
 Ingredient.create(name: "red bull")
 Ingredient.create(name: "pineapple juice")
 
+# a = Cocktail.create(name: "Rum and Coke")
+# b = Cocktail.create(name: "Red Bull Vodka")
+# c = Cocktail.create(name: "Pina colada")
+
+# d = Dose.create(description: "a little bit")
+# e = Dose.create(description: "a lot!")
+# f = Dose.create(description: "as much as your want")
+# g = Dose.create(description: "one sip")
+# h = Dose.create(description: "certain amount")
+# i = Dose.create(description: "tiny tiny")
+
+# d.cocktail = a
+# d.ingredient = 6
+# d.ingredient = 4
+
